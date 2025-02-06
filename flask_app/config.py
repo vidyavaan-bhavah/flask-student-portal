4ca0312b34192064150393f7d252076065fba443
vidyavaan-bhavah <siddhant301201@gmail.com>
2025-02-06 16:05:03 +0530

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "your_secret_key"
+    SECRET_KEY = "-4EfhyePciHtaut_3knNCw5T"
     SQLALCHEMY_DATABASE_URI = "sqlite:///flask-sp.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
